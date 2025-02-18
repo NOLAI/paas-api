e3ca503bbf4a82ca3cc76824ced61be26beba052
Job Doesburg <job.doesburg@gmail.com>
2025-02-18 17:44:07 +0100

    New paths

--- src/paths.rs
+++ src/paths.rs
@@ -6,10 +6,8 @@ pub const SESSIONS_GET: &str = "/sessions";
 pub const SESSIONS_START: &str = "/sessions/start";
 pub const SESSIONS_END: &str = "/sessions/end";
 
-pub mod transcrypt {
-    pub const PSEUDONYMIZE: &str = "/pseudonymize";
-    pub const PSEUDONYMIZE_BATCH: &str = "/pseudonymize_batch";
-    pub const REKEY: &str = "/rekey";
-    pub const REKEY_BATCH: &str = "/rekey_batch";
-    pub const TRANSCRYPT: &str = "/transcrypt";
-}
+pub const PSEUDONYMIZE: &str = "/pseudonymize";
+pub const PSEUDONYMIZE_BATCH: &str = "/pseudonymize_batch";
+pub const REKEY: &str = "/rekey";
+pub const REKEY_BATCH: &str = "/rekey_batch";
+pub const TRANSCRYPT: &str = "/transcrypt";
