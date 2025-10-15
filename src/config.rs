81bdbf86b1c06ea04274b6fdf3824e1a4903b1b4
Job Doesburg <job.doesburg@gmail.com>
2025-10-15 16:31:17 +0200

    Use proper URLs in PAAS-api

--- src/config.rs
+++ src/config.rs
@@ -2,12 +2,13 @@ use crate::status::SystemId;
 use libpep::distributed::key_blinding::BlindedGlobalKeys;
 use libpep::high_level::keys::GlobalPublicKeys;
 use serde::{Deserialize, Serialize};
+use url::Url;
 
 #[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq)]
 /// The details of a single transcryptor
 pub struct TranscryptorConfig {
     pub system_id: SystemId,
-    pub url: String,
+    pub url: Url,
 }
 #[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq)]
 /// The configuration of a PAAS system
