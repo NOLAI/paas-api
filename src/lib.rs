5adc736bee760bf46a64fb645222787baf0b69cc
Job Doesburg <job.doesburg@gmail.com>
2026-01-27 15:46:40 +0100

    Polymorphic paas-api

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 /// The current version of the API
 pub const CURRENT_PROTOCOL_VERSION: &str = env!("CARGO_PKG_VERSION");
 /// The minimal supported compatible version
-pub const MIN_SUPPORTED_VERSION: &str = "0.8.0";
+pub const MIN_SUPPORTED_VERSION: &str = "0.9.0";
 
 /// Transcryptor config
 pub mod config;
